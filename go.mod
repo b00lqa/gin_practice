4b70d87b54ca8af5e72cf954d9f6f40de5b00694
b00lqa <ruth.waelry@yandex.ru>
2023-08-06 18:43:41 +0300

    .env file loading
    Added configuration loading from environment variables

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/bytedance/sonic v1.10.0-rc3 // indirect
+	github.com/caarlos0/env/v9 v9.0.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
 	github.com/fatih/color v1.15.0 // indirect
@@ -15,6 +16,7 @@ require (
 	github.com/go-playground/validator/v10 v10.14.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"log"
+
+	"github.com/caarlos0/env/v9"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
+// Structure containing all configuration variables
+// passed in .env file or system environment.
+type Config struct {
+	MongoDB_URL string `env:"MONGODB_URL"`
+}
+
 func main() {
+	// Load env variables.
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Couldn't load environment variables: %v", err)
+	}
+
+	cfg := Config{}
+	// Parsing environment variables to structure.
+	err = env.Parse(&cfg)
+	if err != nil {
+		log.Fatalf("Coutdn't parse environment variables: %v", err)
+	}
+
 	// Creating router.
 	router := gin.Default()
 	// Loading templates.
