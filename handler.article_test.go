1d1a02249d8680eb764496da5b04d5a86d6dc688
b00lqa <ruth.waelry@yandex.ru>
2023-08-06 17:49:59 +0300

    Created tests
    They do not work for now. Will fix later.

--- handler.article_test.go
+++ handler.article_test.go
@@ -10,11 +10,9 @@ import (
 
 func TestShowIndexPageUnauthenticated(t *testing.T) {
 	r := getRouter(true)
-
 	r.GET("/", showIndexPage)
 
 	req, _ := http.NewRequest("GET", "/", nil)
-
 	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
 		statusOK := w.Code == http.StatusOK
 
@@ -27,5 +25,20 @@ func TestShowIndexPageUnauthenticated(t *testing.T) {
 }
 
 func TestArticleUnauthenticated(t *testing.T) {
+	r := getRouter(true)
+	r.GET("/article/view/:article_id", getArticle)
 
+	articles := getAllArticles()
+	for _, article := range articles {
+		req, _ := http.NewRequest("GET", "/article/view/"+article.ID.String(), nil)
+		testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
+			statusOK := w.Code == http.StatusOK
+
+			p, err := io.ReadAll(w.Body)
+			pageOK := err == nil &&
+				strings.Index(string(p), article.Title) > 0
+
+			return statusOK && pageOK
+		})
+	}
 }
